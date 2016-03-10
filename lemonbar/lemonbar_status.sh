13078dc0be1010d152384e105d83334e4a6f7c48
alexmcnurlin <alemcnurlin@gmail.com>
2016-03-09 20:11:30 -0800

    Work on lemonbar script. It looks sexy right now!

--- lemonbar/lemonbar_status.sh
+++ lemonbar/lemonbar_status.sh
@@ -1,8 +1,75 @@
 #! /usr/bin/env bash
 
+bgcolor=$1
+fgcolor=$2
+accent=$3
+gdcolor=$4
+degcolor=$5
+bdcolor=$6
+
+
+the_time() {
+	date "+ %a %D %T"
+}
+
+workspaces() {
+		get_workspaces=$(i3-msg -t get_workspaces | jq -r 'map(.name) | .[]')
+		current_workspace=$(i3-msg -t get_workspaces | jq -r 'map(select(.focused))[0].name')
+		
+		# Leave this blank
+		output=""
+		# This is the character used to separate workspace icons
+		sep=" "
+
+		for workspace in $get_workspaces; do
+			if [ "$workspace" = "$current_workspace" ]; then
+				output+="%{R} $workspace %{R}"
+			else
+				output+="%{R}%{R} $workspace "
+			fi
+		done
+
+		echo $output
+}
+
+wifi() {
+	# A beautiful mess of grep. Good luck changing this later
+	stat=$(iwconfig wlan0)
+	essid=$(echo $stat | grep -oP "(?<=ESSID:\").*?(?=\")")
+	percent=$(qalc -t "$(echo $stat | grep -oP "(\d+/\d+)") * 100" | grep -oP "^(\d\d\d?$)|(.*?(?=\.))")
+	ip=$(ifconfig wlan0 | grep -oP "(?<=inet addr:).*?\s")
+
+	if [ $percent -lt 50 ]; then
+		color="#$4";
+	elif [ $percent -lt 75 ]; then
+		color="#$5";
+	else
+		color="#$6";
+	fi
+
+	if [ $(echo $stat | grep -oP "ESSID:off/any") ]; then
+		echo  No wireless;
+		exit 0;
+	elif [ "$ip" = "" ]; then
+		ip="No IP";
+		color="#$2";
+	else 
+		ip="IP:$ip";
+	fi;
+
+	echo " %{F$color} $essid: $percent% $ip%{F}"
+}
+
+eth() {
+	status=$(/usr/share/i3blocks/network | head -1)
+	if [ $status != "down" ]; then
+		echo " Eth: $status";
+	else
+		echo "";
+	fi;
+}
+
 while [ 1 ]; do
-	echo "Testing";
-	sleep 1;
+	echo "%{l}$(workspaces $bgcolor $fgcolor $accent $gdcolor $degcolor $bdcolor)%{r}$(eth)$(wifi $bgcolor $fgcolor $accent $gdcolor $degcolor $bdcolor)$(the_time) "
+	sleep .5;
 done
-
-
--- lemonbar/start_lemonbar.sh
+++ lemonbar/start_lemonbar.sh
@@ -4,10 +4,21 @@ monitor2=$2
 outer_gap=$3
 inner_gap=$4
 bar_height=$5
+bgcolor=$6
+fgcolor=$7
+accent=$8
+gdcolor=$9
+degcolor=${10}
+bdcolor=${11}
 
-notify-send -a "test" "$1 $2 $3 $4 $5 $(( monitor1 - 2*outer_gap - 2*inner_gap ))";
 pkill lemonbar
-echo  | lemonbar -p -g $(( monitor1 + 1 ))x$(( bar_height + outer_gap )) &
+
+# This is an invisible bar that makes the tiled windows move out of the way for the actual bar
+echo " " | lemonbar -p -g 2x$(( bar_height + outer_gap ))+$(( monitor1 - 1 )) -B "#00000000" &
+
 # Geometry is {width-2(outer_gap)}x{bar_height}+{offset+outer_gap}+{outer_gap}
-~/.dotfiles/lemonbar/lemonbar_status.sh | lemonbar -d -g $(( monitor1 - 2*outer_gap - 2*inner_gap ))x$bar_height+$(( outer_gap + inner_gap ))+$(( outer_gap + inner_gap )) -B "#88FFFFFF" -F "#FF000000" -p -f FontAwesome:style=Regular &
-~/.dotfiles/lemonbar/lemonbar_status.sh | lemonbar -d -g $(( monitor2 - 2*outer_gap - 2*inner_gap ))x$bar_height+$(( monitor1 + outer_gap + inner_gap ))+$(( outer_gap + inner_gap )) -B "#88FFFFFF" -F "#FF000000" -p -f FontAwesome:style=Regular
+# The bar for the left monitor (monitor1)
+~/.dotfiles/lemonbar/lemonbar_status.sh $bgcolor $fgcolor $accent $gdcolor $degcolor $bdcolor | lemonbar -d -g $(( monitor1 - 2*outer_gap - 2*inner_gap + 2 ))x$bar_height+$(( outer_gap + inner_gap -1 ))+$(( outer_gap + inner_gap )) -B "#FF$bgcolor" -F "#FF$fgcolor" -p -f "Liberation Mono for Powerline:style=Regular" -f "FontAwesome:style=Regular" &
+
+# The bar for the right monitor (monitor2)
+~/.dotfiles/lemonbar/lemonbar_status.sh $bgcolor $fgcolor $accent $gdcolor $degcolor $bdcolor | lemonbar -d -g $(( monitor2 - 2*outer_gap - 2*inner_gap + 2 ))x$bar_height+$(( monitor1 + outer_gap + inner_gap -1 ))+$(( outer_gap + inner_gap )) -B "#88$bgcolor" -F "#FF$fgcolor" -p -f "Liberation Mono for Powerline:style=Regular" -f "FontAwesome:style=Regular"
