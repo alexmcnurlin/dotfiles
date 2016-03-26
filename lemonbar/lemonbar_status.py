2eabcfa1661d2189e28f5bb9074d317691e705b8
alexmcnurlin <alemcnurlin@gmail.com>
2016-03-26 02:37:49 -0700

    Lemonbar: Convert statusbar script to python!

--- lemonbar/lemonbar_status.py
+++ lemonbar/lemonbar_status.py
@@ -23,30 +23,28 @@ sep=" "
 
 def workspaces( bgcolor, fgcolor, accent, gdcolor, degcolor, bdcolor ):
     temp = sub.Popen("i3-msg -t get_workspaces | jq -r 'map(.name) | .[]'", stdout=sub.PIPE, shell=True)
-    get_workspaces = temp.communicate()
+    get_workspaces = temp.communicate()[0].decode("utf-8").replace("\n","")
 
-    #get_workspaces=$(i3-msg -t get_workspaces | jq -r 'map(.name) | .[]')
     temp = sub.Popen("i3-msg -t get_workspaces | jq -r 'map(select(.focused))[0].name'", stdout=sub.PIPE, shell=True)
-    current_workspace = temp.communicate()
-    #current_workspace=$(i3-msg -t get_workspaces | jq -r 'map(select(.focused))[0].name')
+    current_workspace = temp.communicate()[0].decode("utf-8").strip()
     
      #Leave this blank
     output = []
 
     for workspace in get_workspaces:
       if ( workspace == current_workspace ):
-        output.append("%{{F#{0}+u}}%{{U#{0}}} {1} %{{F!u}}".format( accent, workspace ) )
+        output.append("%{{U#{0} F#{0}+u}} {1} %{{F!u}}".format( accent, workspace ) )
       else:
-        output.append("%{{-u}} {0} ".format( workspace ))
+        output.append(" {0} ".format( workspace ))
 
-    print(output)
+    return("".join(output))
 
 
 def eth( fgcolor, bdcolor, eth_ip ):
   if ( eth_ip != "No Address" ):
-    print("%{{F#{0}+u}}%{{U#{0}}} Eth: {2} %{{F!u}}".format(fgcolor, bdcolor, eth_ip))
+    return("%{{F#{0}+u}}%{{U#{0}}} Eth: {2} %{{F!u}}".format(fgcolor, bdcolor, eth_ip))
   else:
-    print("%{{F#{1}+u}}%{{U#{1}}} No Eth %{{F!u}}".format(fgcolor, bdcolor, eth_ip))
+    return("%{{F#{1}+u}}%{{U#{1}}} No Eth %{{F!u}}".format(fgcolor, bdcolor, eth_ip))
 
 
 def wifi( gdcolor, degcolor, bdcolor, fgcolor, wifi_percent, wifi_essid, wifi_ip ):
@@ -57,10 +55,9 @@ def wifi( gdcolor, degcolor, bdcolor, fgcolor, wifi_percent, wifi_essid, wifi_ip
       color=degcolor
     else:
       color=gdcolor
-    print("%{{F#{0}+u}}%{{U#{0}}}  {2}: {1}% IP:{3} %{{F!u}}".format( color, wifi_percent, wifi_essid, wifi_ip ))
+    return("%{{F#{0}+u}}%{{U#{0}}}  {2}: {1}% IP:{3} %{{F!u}}".format( color, wifi_percent, wifi_essid, wifi_ip ))
   else:
-    print("%{{F#{0}+u}}%{{U#{0}}}  No Wifi %{{F!u}}".format( fgcolor, wifi_percent, wifi_essid, wifi_ip ))
-    #echo "%{F#$4+u}%{U#$4}  No Wifi %{F!u}"
+    return("%{{F#{0}+u}}%{{U#{0}}}  No Wifi %{{F!u}}".format( fgcolor, wifi_percent, wifi_essid, wifi_ip ))
 
 
 def cpu( gdcolor, degcolor, bdcolor, cpu_percent ):
@@ -70,7 +67,7 @@ def cpu( gdcolor, degcolor, bdcolor, cpu_percent ):
     color=degcolor
   else:
     color=gdcolor
-  print("%{{F#{0}+u}}%{{U#{0}}}  {1}% %{{F!u}}".format( color, cpu_percent ))
+  return("%{{F#{0}+u}}%{{U#{0}}}  {1}% %{{F!u}}".format( color, cpu_percent ))
 
 
 def temp( gdcolor, degcolor, bdcolor, cpu_temp ):
@@ -80,16 +77,15 @@ def temp( gdcolor, degcolor, bdcolor, cpu_temp ):
     color=degcolor
   else:
     color=bdcolor
-  print("%{{F#{0}+u}}%{{U#{0}}} {1}°C %{{F!u}}".format( color, cpu_temp ))
+  return("%{{F#{0}+u}}%{{U#{0}}} {1}°C %{{F!u}}".format( color, cpu_temp ))
 
 
 def power( gdcolor, degcolor, bdcolor, fgcolor, ac, bat ):
-  #sys.stderr.write("Ac == " + ac)
   if ( ac == "on-line" ):
     if ( int(bat) == 0 ):
-      print("%{{U#{0}+u}}  -- %{{U}}%{{-u}}".format( fgcolor ))
+      return("%{{U#{0}+u}}  -- %{{U}}%{{-u}}".format( fgcolor ))
     else:
-      print("%{{U#{0} F#{0}+u}}  {1}% %{{UF}}%{{-u}}".format( gdcolor, bat ))
+      return("%{{U#{0} F#{0}+u}}  {1}% %{{UF}}%{{-u}}".format( gdcolor, bat ))
   else:
     if ( int(bat) < 20 ):
       icon=""
@@ -108,11 +104,17 @@ def power( gdcolor, degcolor, bdcolor, fgcolor, ac, bat ):
       color=degcolor
     else:
       color=gdcolor
-    print("%{{U#{0} F#{0}+u}} {1} {2}% %{{UF}}%{{-u}}".format( color, icon, bat ))
+    return("%{{U#{0} F#{0}+u}} {1} {2}% %{{UF}}%{{-u}}".format( color, icon, bat ))
 
 
 def volume( fgcolor, degcolor ):
-  vol = call("pactl list sinks | perl -000ne 'if(/#1/){/(\d*(?=%))/; print \"$1\n\"}'", shell=True)
+  try:
+    temp = sub.Popen("pactl list sinks | perl -000ne 'if(/#1/){/(\d*(?=%))/; print \"$1\"}'", stdout=sub.PIPE, shell=True)
+    vol_b = temp.communicate()[0]
+    vol = vol_b.decode("utf-8")
+  except:
+    sys.stderr.write("Error")
+
 
   if ( int(vol) == 0 ):
     icon=""
@@ -121,61 +123,46 @@ def volume( fgcolor, degcolor ):
   else:
     icon=""
 
-  mute = call( "pacmd dump | awk ' $1 == \"set-sink-mute\" {m[$2] = $3}; $1 == \"set-default-sink\" {s = $2}; END {print m[s]}'", shell=True )
+  try:
+     temp = sub.Popen("pacmd dump | awk ' $1 == \"set-sink-mute\" {m[$2] = $3}; $1 == \"set-default-sink\" {s = $2}; END {print m[s]}'", stdout=sub.PIPE, shell=True)
+     mute = temp.communicate()[0]
+  except:
+    sys.stderr.write("Error in mute")
+    mute = "no"
 
   if ( mute == "yes" ):
-    print("%{{U#{0} F#{0}+u}} muted({1}%) %{{UF}}%{{-u}}".format( degcolor, vol ))
+    return("%{{U#{0} F#{0}+u}} muted({1}%) %{{UF}}%{{-u}}".format( degcolor, vol ))
   else:
-    print("%{{U#{0} F#{0}+u}} {2} {1}% %{{UF}}%{{-u}}".format( fgcolor, vol, icon ))
+    return("%{{U#{0} F#{0}+u}} {2} {1}% %{{UF}}%{{-u}}".format( fgcolor, vol, icon ))
 
 # The update interval is controlled through the conky update interval
 #conky = subprocess.Popen("conky -c ~/.dotfiles/lemonbar/conkyrc")
 for line in sys.stdin:
-  #line = conky.stdout.read()
-  output = line.split(";")
+  conky = line.split(";")
 
-  for i in output:
-    sys.stderr.write(i + ", ")
+  the_time     = conky.pop(0)
+  wifi_percent = conky.pop(0)
+  wifi_essid   = conky.pop(0)
+  wifi_ip      = conky.pop(0)
+  eth_ip       = conky.pop(0)
+  cpu_percent  = conky.pop(0)
+  cpu_temp     = conky.pop(0)
+  ac           = conky.pop(0)
+  bat          = conky.pop(0)
 
-  the_time     = line[0]
-  wifi_percent = line[1]
-  wifi_essid   = line[2]
-  wifi_ip      = line[3]
-  eth_ip       = line[4]
-  cpu_percent  = line[5]
-  cpu_temp     = line[6]
-  ac           = line[7]
-  bat          = line[8]
-
-  #workspaces=$(workspaces $bgcolor $fgcolor $accent $gdcolor $degcolor $bdcolor)
   desktops = workspaces( bgcolor, fgcolor, accent, gdcolor, degcolor, bdcolor )
 
-  output = []
-  output.append( volume( fgcolor, degcolor ) )
-  output.append( temp(   fgcolor, degcolor, bdcolor, cpu_temp ) )
-  output.append( cpu(    fgcolor, degcolor, bdcolor, cpu_temp ) )
-  output.append( eth(    fgcolor, degcolor, eth_ip ) )
-  output.append( wifi(   fgcolor, degcolor, bdcolor, fgcolor, wifi_percent, wifi_essid, wifi_ip ) )
-  output.append( power(  fgcolor, degcolor, bdcolor, fgcolor, ac, bat ) )
-  output.append("%{{U#{0}+u}} {1} %{{U!u}}".format( fgcolor, the_time ))
-  #output+="$(volume $fgcolor $degcolor) "
-  #output+="$(temp   $gdcolor $degcolor $bdcolor $cpu_temp) "
-  #output+="$(cpu    $gdcolor $degcolor $bdcolor $cpu_percent) "
-  #output+="$(eth    $fgcolor $bdcolor "$eth_ip") "
-  #output+="$(wifi   $gdcolor $degcolor $bdcolor $fgcolor $wifi_percent $wifi_essid "$wifi_ip") "
-  #output+="$(power  $gdcolor $degcolor $bdcolor $fgcolor $ac $bat) "
-  output+="%{{U#{0}+u}} {1} %{{U!u}} ".format( fgcolor, the_time )
+  str_r_side = []
+  r_side = []
+  r_side.append( str( volume( fgcolor, degcolor )) )
+  r_side.append( str( temp(   fgcolor, degcolor, bdcolor, cpu_temp )) )
+  r_side.append( str( cpu(    fgcolor, degcolor, bdcolor, cpu_percent )) )
+  r_side.append( str( eth(    fgcolor, bdcolor, eth_ip )) )
+  r_side.append( str( wifi(   fgcolor, degcolor, bdcolor, fgcolor, wifi_percent, wifi_essid, wifi_ip )) )
+  r_side.append( str( power(  fgcolor, degcolor, bdcolor, fgcolor, ac, bat )) )
+  r_side.append( str("%{{U#{0}+u}} {1} %{{U!u}}".format( fgcolor, the_time )) )
   
-  left_side = " ".join(output)
+  str_r_side = " ".join(r_side)
 
-  call("notify-send 'hello " + left_side + "'", shell=True)
-  print("%{{l}}{0} %{{r}}{1}".format( workspaces, left_side ))
-  output = []
-
-  result = call("~/.dotfiles/lemonbar/hide_lemonbar.sh")
-  text = result.communicate()[0]
-  returncode = result.returncode
-
-  if returncode:
-    sys.exit(0)
+  print("%{{l}}{0} %{{r}}{1}".format( desktops, str_r_side ), flush=True)
 done
