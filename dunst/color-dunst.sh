e15b12a8e1b0bffcf4a11f6b0312907778481cda
alexmcnurlin <alemcnurlin@gmail.com>
2017-03-27 11:05:04 -0700

    rofi/dmenu/dunst: Add rofi-dmenu to gitignore and add a default configuration that will be copied if rofi-dmenu doesn't exist

--- dunst/color-dunst.sh
+++ dunst/color-dunst.sh
@@ -10,22 +10,29 @@ outer_gap=$7
 bar_height=$8
 border_width=$9
 
-nofity-send "$@"
-sed -e "s/\(geometry = \"[[:digit:]]*x[[:digit:]]*\).*\"/\1-$(( outer_gap + inner_gap ))+$(( outer_gap + bar_height + inner_gap ))\"/" -i ~/.dotfiles/dunst/dunstrc
+if [ ! -e ~/.dotfiles/custom_scripts/rofi-dmenu ]; then
+	cp ~/.dotfiles/custom_scripts/rofi-dmenu-default ~/.dotfiles/custom_scripts/rofi-dmenu
+fi
 
-sed -e "s/\(background = \"#\)[0-9a-fA-F]\{6\}\(\" # low\)/\1$bgcolor\2/g" -i ~/.dotfiles/dunst/dunstrc
-sed -e "s/\(foreground = \"#\)[0-9a-fA-F]\{6\}\(\" # low\)/\1$white\2/g" -i ~/.dotfiles/dunst/dunstrc
-sed -e "s/\(background = \"#\)[0-9a-fA-F]\{6\}\(\" # normal\)/\1$white\2/g" -i ~/.dotfiles/dunst/dunstrc
-sed -e "s/\(foreground = \"#\)[0-9a-fA-F]\{6\}\(\" # normal\)/\1$bgcolor\2/g" -i ~/.dotfiles/dunst/dunstrc
-sed -e "s/\(background = \"#\)[0-9a-fA-F]\{6\}\(\" # critical\)/\1$bdcolor\2/g" -i ~/.dotfiles/dunst/dunstrc
-sed -e "s/\(foreground = \"#\)[0-9a-fA-F]\{6\}\(\" # critical\)/\1$white\2/g" -i ~/.dotfiles/dunst/dunstrc
+if [ ! -e ~/.dotfiles/dunst/dunstrc ]; then
+	cp ~/.dotfiles/dunst/dunstrc_default ~/.dotfiles/dunst/dunstrc
+fi
 
-sed -e "s/\(-bc \"#\)[0-9a-fA-F]\{6\}/\1$bgcolor/g" -i   ~/.dotfiles/custom_scripts/rofi-dmenu
-sed -e "s/\(-bg \"#\)[0-9a-fA-F]\{6\}/\1$bgcolor/g" -i   ~/.dotfiles/custom_scripts/rofi-dmenu
-sed -e "s/\(-fg \"#\)[0-9a-fA-F]\{6\}/\1$fgcolor/g" -i   ~/.dotfiles/custom_scripts/rofi-dmenu
-sed -e "s/\(-hlbg \"#\)[0-9a-fA-F]\{6\}/\1$fgcolor/g" -i ~/.dotfiles/custom_scripts/rofi-dmenu
-sed -e "s/\(-hlfg \"#\)[0-9a-fA-F]\{6\}/\1$bgcolor/g" -i ~/.dotfiles/custom_scripts/rofi-dmenu
+	sed -e "s/\(geometry = \"[[:digit:]]*x[[:digit:]]*\).*\"/\1-$(( outer_gap + inner_gap ))+$(( outer_gap + bar_height + inner_gap ))\"/" -i ~/.dotfiles/dunst/dunstrc
 
-sed -e "s/\(separator_height = \)[[:digit:]]*/\1$border_width/g" -i ~/.dotfiles/dunst/dunstrc
-sed -e "s/\(separator_color = \"#\)[0-9a-fA-F]\{6\}\(\"\)/\1$fgcolor\2/g" -i ~/.dotfiles/dunst/dunstrc
-#sed -e "s/\(separator_color = \)\"#[[:digit:]]*\"/\1\"#$fgcolor\"/" -i ~/.dotfiles/dunst/dunstrc
+	sed -e "s/\(background = \"#\)[0-9a-fA-F]\{6\}\(\" # low\)/\1$bgcolor\2/g" -i ~/.dotfiles/dunst/dunstrc
+	sed -e "s/\(foreground = \"#\)[0-9a-fA-F]\{6\}\(\" # low\)/\1$white\2/g" -i ~/.dotfiles/dunst/dunstrc
+	sed -e "s/\(background = \"#\)[0-9a-fA-F]\{6\}\(\" # normal\)/\1$white\2/g" -i ~/.dotfiles/dunst/dunstrc
+	sed -e "s/\(foreground = \"#\)[0-9a-fA-F]\{6\}\(\" # normal\)/\1$bgcolor\2/g" -i ~/.dotfiles/dunst/dunstrc
+	sed -e "s/\(background = \"#\)[0-9a-fA-F]\{6\}\(\" # critical\)/\1$bdcolor\2/g" -i ~/.dotfiles/dunst/dunstrc
+	sed -e "s/\(foreground = \"#\)[0-9a-fA-F]\{6\}\(\" # critical\)/\1$white\2/g" -i ~/.dotfiles/dunst/dunstrc
+
+	sed -e "s/\(-bc \"#\)[0-9a-fA-F]\{6\}/\1$bgcolor/g" -i   ~/.dotfiles/custom_scripts/rofi-dmenu
+	sed -e "s/\(-bg \"#\)[0-9a-fA-F]\{6\}/\1$bgcolor/g" -i   ~/.dotfiles/custom_scripts/rofi-dmenu
+	sed -e "s/\(-fg \"#\)[0-9a-fA-F]\{6\}/\1$fgcolor/g" -i   ~/.dotfiles/custom_scripts/rofi-dmenu
+	sed -e "s/\(-hlbg \"#\)[0-9a-fA-F]\{6\}/\1$fgcolor/g" -i ~/.dotfiles/custom_scripts/rofi-dmenu
+	sed -e "s/\(-hlfg \"#\)[0-9a-fA-F]\{6\}/\1$bgcolor/g" -i ~/.dotfiles/custom_scripts/rofi-dmenu
+
+	sed -e "s/\(separator_height = \)[[:digit:]]*/\1$border_width/g" -i ~/.dotfiles/dunst/dunstrc
+	sed -e "s/\(separator_color = \"#\)[0-9a-fA-F]\{6\}\(\"\)/\1$fgcolor\2/g" -i ~/.dotfiles/dunst/dunstrc
+	#sed -e "s/\(separator_color = \)\"#[[:digit:]]*\"/\1\"#$fgcolor\"/" -i ~/.dotfiles/dunst/dunstrc
