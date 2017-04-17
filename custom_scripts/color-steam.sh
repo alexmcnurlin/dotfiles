22f9825d4a13bed0701358bbf81105fc5a075fc1
alexmcnurlin <alemcnurlin@gmail.com>
2017-04-17 14:49:07 -0700

    Color-steam: Remove unnecesary notify-send

--- custom_scripts/color-steam.sh
+++ custom_scripts/color-steam.sh
@@ -10,7 +10,6 @@ border_width=$7
 
 myfile=~/.local/share/Steam/skins/Metro\ for\ Steam\ 4.2.3/custom.styles
 if [ -e "$myfile" ]; then
-  notify-send "#$fgcolor"
   sed -e "s/\(Focus=\"\)[0-9]\{1,3\} [0-9]\{1,3\} [0-9]\{1,3\}\( 255\"\)/\1$(hexrgb $fgcolor)\2/g" -i "$myfile"
   # Focus_T="0 114 198 30.6"
 fi
